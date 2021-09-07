84fa54a35b2b9c12b0e05a8daa51cbfe19bcbf19
Aadit Rahul Kamat <aadit.k12@gmail.com>
2021-09-07 23:14:20 +0800

    Add calculation logic

---
+++ public/js/index.js
@@ -0,0 +1,40 @@
+console.log(document);
+
+function calculate(first, second, operation) {
+    if (operation === '+') {
+        return first + second;
+    }
+    else if (operation === '-') {
+        return first - second;
+    }
+    else if (operation === '/') {
+        return first / second;
+    }
+    else if (operation === 'x') {
+        return first * second;
+    }
+    else if (operation === '^') {
+        return first ^ second;
+    }
+    else {
+        return first % second;
+    }
+}
+
+function onclick() {
+    console.log('Button clicked');
+    const first = parseInt(document.getElementById('first'));
+    const second = parseInt(document.getElementById('second'));
+    const selectedIndex = document.getElementById('operations').selectedIndex;
+    const children = document.getElementById('operations').children;
+    const selectedElement = children[selectedIndex];
+    const operation = selectedElement.value;
+    const result = calculate(first, second, operation);
+    document.getElementById('result').value = result;
+}
+
+window.onload = function() {
+    console.log('Document loaded');
+    const button = document.querySelector('button');
+    button.onclick = onclick;
+}
