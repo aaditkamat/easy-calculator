28f602f236f427c6ccfe30c28f793daae3bb302e
Aadit Rahul Kamat <aadit.k12@gmail.com>
2021-09-07 23:47:02 +0800

    Update port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
  
 app.use('/', express.static('public'));
  
-app.listen(3000);
+app.listen(PORT);
